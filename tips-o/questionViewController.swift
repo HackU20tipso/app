97954ae74b1213348da2d9c74ddef6cb2ccf5841
fujimotoayaka <fujimotoayaka.happy@gmail.com>
2020-08-26 13:36:14 +0900

    初期画面作った

--- tips-o/questionViewController.swift
+++ tips-o/questionViewController.swift
@@ -19,8 +19,12 @@ class questionViewController: UIViewController,UIPickerViewDelegate,UIPickerView
     
     @IBOutlet weak var targetTextField: UITextField!
     
+    
     @IBOutlet weak var targetpick: UIPickerView!
-    let compos = [["女性","男性","どちらでもない"]]
+    @IBOutlet weak var themepick: UIPickerView!
+    let compos = [["10代","20代","30代","40代","それ以上"],["女性","男性","どちらでもない"]]
+    
+    let compos2 = [["心理系","情報系","食物系","生物系"]]
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return compos.count
@@ -32,8 +36,12 @@ class questionViewController: UIViewController,UIPickerViewDelegate,UIPickerView
     }
     
     func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
-    
-            return 120
+        if component==0{
+            return 100
+        }else{
+            return 130
+        }
+            
         
     }
     
@@ -42,24 +50,26 @@ class questionViewController: UIViewController,UIPickerViewDelegate,UIPickerView
         return item
     }
     
+    
+    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         let item = compos[component][row]
         
         print("\(item)が選ばれた")
         
         let row1 = pickerView.selectedRow(inComponent: 0)
-        /*let row2 = pickerView.selectedRow(inComponent: 1)
+        let row2 = pickerView.selectedRow(inComponent: 1)
         
-        let st = (row1,row2)*/
+        let st = (row1,row2)
         
-        print("現在選択されている行番号\(row1)")
+        print("現在選択されている行番号\(st)")
         
         let item1 = self.pickerView(pickerView, titleForRow: row1, forComponent: 0)
-        /*let item2 = self.pickerView(pickerView, titleForRow: row2, forComponent: 1)
+        let item2 = self.pickerView(pickerView, titleForRow: row2, forComponent: 1)
         
-        let st2 = (item1!,item2!)*/
+        let st2 = (item1!,item2!)
         
-        print("現在選択されている項目名\(String(describing: item1))")
+        print("現在選択されている項目名\(st2)")
         
     }
     var docRef2: DocumentReference!
