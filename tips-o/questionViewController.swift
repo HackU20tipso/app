e26c808db97e0d7c4afc2a4e7f35f8cc56036457
fujimotoayaka <fujimotoayaka.happy@gmail.com>
2020-08-26 12:05:25 +0900

    pick作った

--- tips-o/questionViewController.swift
+++ tips-o/questionViewController.swift
@@ -8,15 +8,60 @@ import FirebaseAnalytics
 
 
 
-class questionViewController: UIViewController  {
+class questionViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource  {
 
     
     @IBOutlet weak var quesLabel: UILabel!
     @IBOutlet weak var urlTextField: UITextField!
     @IBOutlet weak var themeTextField: UITextField!
     @IBOutlet weak var typeTextField: UITextField!
+    
+    
     @IBOutlet weak var targetTextField: UITextField!
     
+    @IBOutlet weak var targetpick: UIPickerView!
+    let compos = [["女性","男性","どちらでもない"]]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return compos.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        let compo = compos[component]
+        return compo.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+    
+            return 120
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let item = compos [component][row]
+        return item
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let item = compos[component][row]
+        
+        print("\(item)が選ばれた")
+        
+        let row1 = pickerView.selectedRow(inComponent: 0)
+        /*let row2 = pickerView.selectedRow(inComponent: 1)
+        
+        let st = (row1,row2)*/
+        
+        print("現在選択されている行番号\(row1)")
+        
+        let item1 = self.pickerView(pickerView, titleForRow: row1, forComponent: 0)
+        /*let item2 = self.pickerView(pickerView, titleForRow: row2, forComponent: 1)
+        
+        let st2 = (item1!,item2!)*/
+        
+        print("現在選択されている項目名\(String(describing: item1))")
+        
+    }
     var docRef2: DocumentReference!
     
     @IBAction func saveTapped2(_ sender: Any) {
@@ -24,7 +69,10 @@ class questionViewController: UIViewController  {
         guard let quesText = quesLabel.text, !quesText.isEmpty else {return}
         guard let urlAuther = urlTextField.text, !urlAuther.isEmpty else {return}
         guard let themeAuther = themeTextField.text, !themeAuther.isEmpty else {return}
-        guard let targetAuther = targetTextField.text, !targetAuther.isEmpty else {return}
+        guard let targetAuther =
+        /*targetpick.textInputContextIdentifier,
+        !targetAuther.isEmpty else{return}*/
+        targetTextField.text, !targetAuther.isEmpty else {return}
         
         
         let DataToSave2 : [String: Any] = ["url" : urlAuther , "theme" : themeAuther, "target" : targetAuther]
@@ -55,6 +103,8 @@ class questionViewController: UIViewController  {
     override func viewDidLoad() {
         super.viewDidLoad()
       
+        targetpick.delegate = self
+        targetpick.dataSource = self
         docRef2 = Firestore.firestore().collection("QuestionnareData").document("question")
            // Do any additional setup after loading the view.
         
