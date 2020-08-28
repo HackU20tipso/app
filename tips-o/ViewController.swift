84cf5aa94099aa1a79062f7f473d8ff9788fecd3
fujimotoayaka <fujimotoayaka.happy@gmail.com>
2020-08-28 13:25:28 +0900

    ログイン作った

--- tips-o/ViewController.swift
+++ tips-o/ViewController.swift
@@ -12,15 +12,34 @@ import FirebaseFirestore
 
 var db: Firestore!
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     
 
+    @IBOutlet weak var UserNameTextField: UITextField!
+    @IBOutlet weak var UserIdTextField: UITextField!
+    @IBOutlet weak var ErrorLabel: UILabel!
+    @IBAction func LoginButton(_ sender: Any) {
+        if UserIdTextField.text! == "123" && UserNameTextField.text! == "ayaka" {
+            // 移動先のビューコントローラを参照する
+            let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "tabPage")
+            // トランジションの映像効果を指定する
+            nextVC?.modalTransitionStyle = .flipHorizontal
+            // シーンを移動する
+            present(nextVC!, animated: true, completion: nil)
+            
+            print("終わった")
+        }else{
+            ErrorLabel.text! = "NameとIDが違います"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // [START setup]
-        
+        UserNameTextField.delegate = self
+        UserIdTextField.delegate = self
+
         
         
         let settings = FirestoreSettings()
@@ -30,6 +49,13 @@ class ViewController: UIViewController {
         db = Firestore.firestore()
     }
 
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+           view.endEditing(true)
+           print(UserNameTextField.text!)
+           print(UserIdTextField.text!)
+           guard let nameText = UserIdTextField.text, !nameText.isEmpty else {return false}
+           return false
+       }
+          
 }
 
