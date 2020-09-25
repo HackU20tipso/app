42fc074f0c1245e9a19340e032b595bfe1b9d6a0
si-tm <kumomaya@gmail.com>
2020-09-25 22:04:08 +0900

    ちょい変え

---
+++ tips-o/questionlistViewController.swift
@@ -0,0 +1,30 @@
+//
+//  questionlistViewController.swift
+//  tips-o
+//
+//  Created by 百塚真弥 on 2020/09/24.
+//  Copyright © 2020 百塚真弥. All rights reserved.
+//
+
+import UIKit
+
+class questionlistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
