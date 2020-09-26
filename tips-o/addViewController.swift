eb795d159b1702aa5b2271e8c35e42077c98b107
si-tm <kumomaya@gmail.com>
2020-09-26 14:21:29 +0900

    あと少し！

--- tips-o/addViewController.swift
+++ tips-o/addViewController.swift
@@ -1,58 +1,88 @@
-//  AddController.swift
+//
+//  ViewController.swift
+//  insects_tableView
+//
+//  Created by yoshiyuki oshige on 2018/08/17.
+//  Copyright © 2018年 yoshiyuki oshige. All rights reserved.
+//
+
 import UIKit
-import WebKit
 
-//変数の設置
-var TodoKobetsunonakami = [String]()
+// テーブルビューに表示するデータ
+let qsectionTitle = ["チョウ目"]
+let qsection0 = [("キタテハ","タテハチョウ科"),("クロアゲハ","アゲハチョウ科")]
+let qtableData = [qsection0]
 
-class addViewController: UIViewController {
-
-    //@IBOutlet var webView: WKWebView!
+class addViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    // シーン移動の際に設定されるWebデータ
-    var data:(name:String, url:String)?
-    @IBOutlet var webVIew2: WKWebView!
-    
+    let me = AppUser()
     let fromAppDelegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("ここです！！！！！")
+        print("ここですよ！！！！！")
         print(fromAppDelegate.ThisUsername)
         //fromAppDelegate.Thispassword = "うふ"
         print(fromAppDelegate.Thispassword)
-         
         
-        // dataが設定されていればwebDataに代入する
-        guard let webData = data else {
-            return
+        if let user_name = fromAppDelegate.ThisUsername{
+            print("どうですかね")
+            print(user_name)
         }
-       
+        if let user_id = fromAppDelegate.Thispassword{
+            print(user_id)
+        }
         
-        // Do any additional setup after loading the view.
-        // 表示するWebページのURLRequestを作る
-        let myURL = URL(string: webData.url)
-        let myRequest = URLRequest(url: myURL!)
-        print(myRequest)
-        // Webを読み込む
-        
-        //webView.load(myRequest)
-        
+        // テーブルビューを作る
+        let myTableView:UITableView!
+        myTableView = UITableView(frame: view.frame, style: .grouped)
+        // テーブルビューのデリゲートを設定する
+        myTableView.delegate = self
+        // テーブルビューのデータソースを設定する
+        myTableView.dataSource = self
+        // テーブルビューを表示する
+        view.addSubview(myTableView)
     }
     
-   
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
-    
-    
+    /*　UITableViewDataSourceプロトコル */
+    // セクションの個数を決める
+    func numberOfSections(in qtableView: UITableView) -> Int {
+        print("\(qsectionTitle.count)")
+        return qsectionTitle.count
+    }
+
+    // セクションごとの行数を決める
+    func tableView(_   qtableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let qsectionData = qtableData[section]
+        return qsectionData.count
+    }
+
+    // セクションのタイトルを決める
+    func tableView(_ qtableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return qsectionTitle[section]
+    }
+
+    // セルを作る
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        let qsectionData = qtableData[(indexPath as NSIndexPath).section]
+        let cellData = qsectionData[(indexPath as NSIndexPath).row]
+        cell.textLabel?.text = cellData.0
+        cell.detailTextLabel?.text = cellData.1
+        return cell
+    }
+
+    /* UITableViewDelegateデリゲートメソッド */
+    // 行がタップされると実行される
+    func qtableView(_ qtableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let title = sectionTitle[indexPath.section]
+        let sectionData = qtableData[indexPath.section]
+        let cellData = sectionData[indexPath.row]
+        print("\(title)\(cellData.1)")
+        print("\(cellData.0)")
+    }
+
 }
+
+
